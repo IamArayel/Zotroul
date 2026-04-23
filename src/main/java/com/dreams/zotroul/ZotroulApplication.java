2934bf4ee31133c470b9ccf71c81158729754eec
krmultipro <krmulti_pro@pm.me>
2026-04-23 09:59:27 +0400

    architecture projet

---
+++ src/main/java/com/dreams/zotroul/ZotroulApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.zotroul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZotroulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZotroulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreams/zotroul/ZotroulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreams.zotroul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZotroulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
