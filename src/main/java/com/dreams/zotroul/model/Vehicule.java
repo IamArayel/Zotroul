53a4761c886ebffd628423087741c6c726501d7f
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2026-04-23 16:23:38 +0400

    Feature : Ajout entité Vélo + Controller + Repository

--- src/main/java/com/dreams/zotroul/model/Vehicule.java
+++ src/main/java/com/dreams/zotroul/model/Vehicule.java
@@ -9,10 +9,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", scope = Vehicule.class)
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Vehicule {
 
     @Id
---
+++ src/main/java/com/dreams/zotroul/repository/VeloRepository.java
@@ -0,0 +1,8 @@
+package com.dreams.zotroul.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dreams.zotroul.model.Velo;
+
+public interface VeloRepository extends JpaRepository<Velo, Long> {
+    
+}
