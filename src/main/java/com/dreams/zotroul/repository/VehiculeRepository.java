d40fd773cff43af1be13567433e2fb5859a28fab
IAmArayel <arayel@hotmail.fr>
2026-04-23 13:13:10 +0400

    feat(vehicule): Ajouter l'API de gestion des véhicules
    - Implémente les opérations CRUD
    - Ajoute le contrôleur REST Vehicule
    - Crée le repository JPA Vehicule

---
+++ src/main/java/com/dreams/zotroul/repository/VehiculeRepository.java
@@ -0,0 +1,7 @@
+package com.dreams.zotroul.repository;
+
+import com.dreams.zotroul.model.Vehicule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
+}
