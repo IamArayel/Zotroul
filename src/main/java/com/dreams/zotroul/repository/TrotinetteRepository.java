17ff19605cde46f1a6ab4cd6e8c9b4636d435241
krmultipro <krmulti_pro@pm.me>
2026-04-24 09:19:09 +0400

    creation du controller trotinette

---
+++ src/main/java/com/dreams/zotroul/repository/TrotinetteRepository.java
@@ -0,0 +1,8 @@
+package com.dreams.zotroul.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dreams.zotroul.model.Trotinette;
+
+public interface TrotinetteRepository extends JpaRepository<Trotinette, Long> {
+}
